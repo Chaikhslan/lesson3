71a31261d3c84feba90c10dfe70b3cf56ba424b0
abay <shapagatin.a@chocomart.kz>
2020-11-12 17:40:19 +0600

    second commit

--- app/src/main/java/com/example/translator/MainActivity.kt
+++ app/src/main/java/com/example/translator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.translator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.example.translator.R.menu
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
